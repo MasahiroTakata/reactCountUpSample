1c7aca91154dd9780c86fd2b6999c20d4e0ecab2
Masahiro Takata <takata005581016@gmail.com>
2023-06-22 22:42:19 +0900

    reactのカウントアップ作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  // 更新関数
+  const [count, setCount] = useState(0);
+  const onClickButton = () => {
+    setCount(count + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+      <p>You clicked {count} times</p>
+      <button onClick={onClickButton}>
+        Click me
+      </button>
     </div>
   );
 }
